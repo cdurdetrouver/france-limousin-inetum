b2673a2c2b96f4f5bcb55b322f887d0f24fe95a2
Samuel_Guillot <samuelguillot75@gmail.com>
2025-02-24 19:57:25 +0100

    div

--- backend/srcs/main.py
+++ backend/srcs/main.py
@@ -4,69 +4,29 @@ import matplotlib.pyplot as plt
 import io
 import pandas as pd
 import numpy as np
+import pickle
+from sklearn.linear_model import LinearRegression
+from pred import pred
+import json 
 
 app = Flask(__name__)
+
 
 @app.route("/prediction_id", methods=["POST"])
 def make_prediction_by_id():
     data = request.get_json()
     
-    if not data or "animal_id" not in data:
+    if not data or "id" not in data:
         return jsonify({"error": "ID d'animal manquant"}), 400
-    
-    # Charger les données
-    try:
-        df_taureaux = pd.read_csv("./data/DATA_TAUREAUX_ANO.txt", sep="\t")  # Vérifie le séparateur
-    except Exception as e:
-        return jsonify({"error": f"Erreur lors du chargement des données : {str(e)}"}), 500
-    
-    animal_id = data["animal_id"]
 
-    # Vérifier si l'ID existe dans le DataFrame
-    animal_data = df_taureaux[df_taureaux["ID_ANIMAL"] == animal_id]
-    
-    if animal_data.empty:
-        return jsonify({"error": "ID d'animal non trouvé"}), 404
-    
-    # Récupérer toutes les colonnes de l'animal
-    variables_animal = animal_data.iloc[0].to_dict()
+    animal_id = int(data["id"])
 
-    # Remplacer les NaN par None pour éviter les erreurs JSON
-    variables_animal = {k: (v if pd.notna(v) else None) for k, v in variables_animal.items()}
-     
-    # Appels des prédictions
-    result = {
-        "variables": variables_animal,
-        "predictions": {
-            "p_libeco2": prediction_libeco2(),
-            "p_libeco3": prediction_libeco3(),
-            "p_enchere": prediction_enchere(),
-            "p_PN": prediction_PNC(),
-            "p_PN210": prediction_PN210(),
-        }
-    }
+    result = pred(animal_id)
+    result = {key: str(value) for key, value in result.items()}
+    result = json.dumps(result)
+    print(result)
+    return result
 
-    # Retour des résultats
-    return jsonify(result)
-
-
-
-def prediction_libeco2():
-    
-    
-def prediction_libeco3():
-    
-    
-def prediction_enchere():
-
-
-def prediction_PNC():
-    
-    
-def prediction_PN210():
-    
-    
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
